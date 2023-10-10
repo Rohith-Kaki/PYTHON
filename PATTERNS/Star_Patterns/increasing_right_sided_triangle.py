ea4c48f1db5bdaaeb350a6da48616fb698ea928a
Rohith Kaki <rohithkaki11@gmail.com>
2023-10-10 18:27:17 +0530

    trianle pattern

---
+++ PATTERNS/Star_Patterns/increasing_right_sided_triangle.py
@@ -0,0 +1,9 @@
+def increasing_right_sided_triangle(n):
+    for i in range(n):
+        for j in range(n-i-1):
+            print(" ",end=" ")
+        for j in range(i+1):
+            print("*",end=" ")
+        print()
+n = int(input("Ener number of rows:"))
+increasing_right_sided_triangle(n)
