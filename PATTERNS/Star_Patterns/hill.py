5f9269d858398ec690fb9857ae9deff58bf8aa76
Rohith Kaki <rohithkaki11@gmail.com>
2023-10-11 18:24:41 +0530

    Hill with Stars

---
+++ PATTERNS/Star_Patterns/hill.py
@@ -0,0 +1,11 @@
+def hill(n):
+    for i in range(n):
+        for j in range(n-i-1):
+            print(" ",end=" ")
+        for j in range(i+1):
+            print("*",end=" ")
+        for j in range(i):
+            print("*",end=" ")
+        print()
+n = int(input("Entter number of rows:"))
+hill(n)
