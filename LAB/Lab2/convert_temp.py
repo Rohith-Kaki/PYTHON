024a7fe67e5d27f5231ef848418988ceb5836617
Rohith Kaki <rohithkaki11@gmail.com>
2023-10-19 19:37:23 +0530

    Converts temperature

--- LAB/Lab2/convert_temp.py
+++ LAB/Lab2/convert_temp.py
@@ -1,5 +1,5 @@
 #WAP to convert celsius value to fahrenheit and print the value
 
-t = int(input("Enter celsius:"))
+t = int(input("Enter celsius:"))          #Takes input from user
 f = (t*(9/5))+32
-print("Fahrenheit is:",f) 
+print("Fahrenheit is:",f)                 #Prints the temperature in fahrenheit
