e322c5c53587a4ed7b397a259f647627f4928d3a
Rohith <143797376+Rohith-Kaki@users.noreply.github.com>
2023-10-23 08:18:44 +0530

    Update reverse_hill.py

--- PATTERNS/Star_Patterns/reverse_hill.py
+++ PATTERNS/Star_Patterns/reverse_hill.py
@@ -1,5 +1,5 @@
 def reverse_hill(n):
-    for i in range(n):
+    for i in range(n):   #i for rows and j for colums and spaces.
         for j in range(i):
             print(" ",end=" ")
         for j in range(n-i):
