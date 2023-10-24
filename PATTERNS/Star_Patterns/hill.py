43499106bb1158546cf6236f9585bfe94ba44983
Rohith <143797376+Rohith-Kaki@users.noreply.github.com>
2023-10-24 13:39:00 +0530

    Update hill.py

--- PATTERNS/Star_Patterns/hill.py
+++ PATTERNS/Star_Patterns/hill.py
@@ -1,6 +1,6 @@
 def hill(n):
     for i in range(n):
-        for j in range(n-i-1):
+        for j in range(n-i-1): # i for rows j for coloums and spaces.
             print(" ",end=" ")
         for j in range(i+1):
             print("*",end=" ")
