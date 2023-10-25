34152c7968532e2392e48751d06ba699d067be56
Rohith <143797376+Rohith-Kaki@users.noreply.github.com>
2023-10-25 19:15:23 +0530

    Update decreasing_right_sided_triangle.py

--- PATTERNS/Star_Patterns/decreasing_right_sided_triangle.py
+++ PATTERNS/Star_Patterns/decreasing_right_sided_triangle.py
@@ -1,5 +1,5 @@
 def decreasing_right_sided_triangle(n):
-    for i in range(n):
+    for i in range(n):  #i for rows and j for coloums.
         for j in range(i):
             print(" ",end=" ")
         for j in range(n-i):
