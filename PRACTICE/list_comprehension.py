db43848787635e1a6277eb10f28a3936378c90ae
Rohith Kaki <rohithkaki11@gmail.com>
2023-11-09 16:23:48 +0530

    list_comprehension

---
+++ PRACTICE/list_comprehension.py
@@ -0,0 +1,7 @@
+def even_numbers(first, last):
+  return [p for p in range(first,last) if p%2 == 0]
+
+
+print(even_numbers(4, 14)) # Should print [4, 6, 8, 10, 12]
+print(even_numbers(0, 9))  # Should print [0, 2, 4, 6, 8]
+print(even_numbers(2, 7))  # Should print [2, 4, 6]
